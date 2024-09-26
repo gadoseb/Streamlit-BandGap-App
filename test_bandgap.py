3fc799f6048735fd94c1890b760b694de82104b5
gadoseb <sebastianogadolini@outlook.com>
2024-09-26 14:20:26 +0100

    fdsfds

--- test_bandgap.py
+++ test_bandgap.py
@@ -276,7 +276,7 @@ def main():
         st.pyplot(fig)
 
         # Plot Reflectance Spectrum
-        st.subheaderheader("Reflectance Spectrum:")
+        st.subheade("Reflectance Spectrum:")
         fig, ax = plt.subplots()
         ax.plot(wavelength, reflectance, label='Reflectance', color='black')
         ax.set_xlabel('Wavelength')
